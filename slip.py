022d38e0d1e9d808031a43b9bf44a1bb9981d9de
João <hmtjoao@gmail.com>
2025-02-18 07:11:33 -0300

    fim

--- slip.py
+++ slip.py
@@ -43,6 +43,7 @@ class Enlace:
     def __init__(self, linha_serial):
         self.linha_serial = linha_serial
         self.linha_serial.registrar_recebedor(self.__raw_recv)
+        self.buffer = b""
 
     def registrar_recebedor(self, callback):
         self.callback = callback
@@ -51,7 +52,19 @@ class Enlace:
         # TODO: Preencha aqui com o código para enviar o datagrama pela linha
         # serial, fazendo corretamente a delimitação de quadros e o escape de
         # sequências especiais, de acordo com o protocolo CamadaEnlace (RFC 1055).
-        pass
+
+        # Realiza a substituição das sequências de escape
+        datagrama = datagrama.replace(b'\xDB', b'\xDB\xDD')
+        datagrama = datagrama.replace(b'\xC0', b'\xDB\xDC')
+        
+        # Insere o byte de início no começo do datagrama
+        datagrama_com_inicio = b'\xC0' + datagrama
+        
+        # Insere o byte de fim no final do datagrama
+        datagrama_com_inicio_e_fim = datagrama_com_inicio + b'\xC0'
+        
+        # Envia o datagrama para a linha serial
+        self.linha_serial.enviar(datagrama_com_inicio_e_fim)
 
     def __raw_recv(self, dados):
         # TODO: Preencha aqui com o código para receber dados da linha serial.
@@ -61,4 +74,18 @@ class Enlace:
         # vir quebrado de várias formas diferentes - por exemplo, podem vir
         # apenas pedaços de um quadro, ou um pedaço de quadro seguido de um
         # pedaço de outro, ou vários quadros de uma vez só.
-        pass
+        dados = dados.split(b'\xC0')
+        while dados:
+            self.buffer += dados.pop(0)
+            if len(dados) > 0 and len(self.buffer) > 0:
+                self.buffer = self.buffer.replace(b'\xDB\xDC', b'\xC0').replace(b'\xDB\xDD', b'\xDB')
+                try:
+                    self.callback(self.buffer)
+                except:
+                      # ignora a exceção, mas mostra na tela
+                    import traceback
+                    traceback.print_exc()
+                finally:
+                    # faça aqui a limpeza necessária para garantir que não vão sobrar
+                    # pedaços do datagrama em nenhum buffer mantido por você
+                    self.buffer = b''
